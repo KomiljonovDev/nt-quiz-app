bd0ce056c6d01be2c4517934b5bfbc2add932b24
Komiljonov O <uzokdeveloper@gmail.com>
2025-01-24 18:04:20 +0500

    patch: Share quiz

--- resources/views/dashboard/quizzes.php
+++ resources/views/dashboard/quizzes.php
@@ -107,7 +107,7 @@
                                 </div>
                                 <div class="flex justify-between">
                                     <a href="/dashboard/quizzes/${quiz.id}/update" class="text-indigo-600 hover:text-indigo-800">Edit</a>
-                                    <button class="text-green-600 hover:text-green-800">View Results</button>
+                                    <button class="text-green-600 hover:text-green-800" onclick="copyContent('${quiz.unique_value}')">Share</button>
                                     <button class="text-red-600 hover:text-red-800" onclick="deleteQuiz(${quiz.id})">Delete</button>
                                 </div>
                             </div>
@@ -129,6 +129,15 @@
                     .catch((error) => {
                         alert("Internetingga qarasangang bo'lmaydimi? :)");
                     });
+            }
+        }
+        const copyContent = async (uniqueValue) => {
+            try {
+                uniqueValue = '<?php echo $_ENV['APP_URL']?>' + '/take-quiz/' + uniqueValue;
+                await navigator.clipboard.writeText(uniqueValue);
+                alert('Content copied to clipboard');
+            } catch (err) {
+                console.error('Failed to copy: ', err);
             }
         }
     </script>
